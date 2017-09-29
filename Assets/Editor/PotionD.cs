32e4dd8bb34573e01303130fa81b8f45426acfb3
Arthur Gomes <arthulek@gmail.com>
2017-09-29 11:16:59 -0300

    AV3_TPJ2_3BI
    Trabalho TPJ2

---
+++ Assets/Editor/PotionD.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(PotionSelectorAttribute))]
+
+public class PotionD : PropertyDrawer {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        int index = 0;
+        string[] d = new string[PotionDB.Instance.potions.Length];
+        for (int i = 0;  i < PotionDB.Instance.potions.Length; i++)
+        {
+            d[i] = PotionDB.Instance.potions[i].name;
+            if (d[i] == property.stringValue)
+            {
+                index = i;
+            }
+
+
+
+
+
+
+        }
+        index = EditorGUI.Popup(position, index, d);
+        property.stringValue = d[index];
+
+    }
+
+}
---
+++ Assets/Scripts/TestBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class PotionSelectorAttribute : PropertyAttribute
+{
+}
+
+public class TestBehaviour : MonoBehaviour
+{
+	[PotionSelector]
+	public string potionName;
+
+	public void Start()
+	{
+		// PotionDB.Instance pra acessar o "banco de dados" de pocoes
+		// Imprimir o quanto aquela potion recupera
+
+        for (int i = 0; i < PotionDB.Instance.potions.Length; i++)
+        {
+            if (PotionDB.Instance.potions[i].name == potionName)
+            {
+                Debug.Log(PotionDB.Instance.potions[i].heal);
+            }
+        }
+	}
+}
